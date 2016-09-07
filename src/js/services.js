f257986d9d165617bbbe261f8d2dd8e066501c29
Gerard del Castillo <gerarddelcastillo@gmail.com>
2016-09-08 00:08:59 +0200

    auto hostname

--- src/js/services.js
+++ src/js/services.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('services', [])
-.factory('API',['$http', '$q', function($http, $q){
-	var API_URL = "http://localhost:3000";
+.factory('API',['$http', '$q', '$window', function($http, $q, $window){
+	var API_URL = "http://"+$window.location.hostname +":"+ $window.location.port;
 	var service = {};
 	service.getNetworks = function(){
 		return $http.get(API_URL+"/networks");
